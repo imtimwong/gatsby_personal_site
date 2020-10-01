768b3bb09ee2e8671ba12780246e09c0f374aa8b
imtimwong <20898842+imtimwong@users.noreply.github.com>
2020-10-01 14:08:14 +0800

    add email link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,11 +4,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { FaEnvelope } from 'react-icons/fa';
+
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact" />
-    <h1>Hi from the contact page</h1>
-    <p>Welcome to contact page</p>
+    <h1>Wanna get in touch?</h1>
+
+    <p>Feel free to drop me an email at :<a style = {{fontSize: "1.2em", position: "relative", top: "5px", left: "5px", color:"#4b6ed0"}} href="mailto:bytimwong@gmail.com" 
+    target="_blank" rel="noopener noreferrer"><FaEnvelope /></a></p>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
